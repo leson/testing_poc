8c715dd9b5e2f55e4f82c82fbc3092baf64113c9
leson <i.leson@163.com>
2017-05-09 23:02:41 +0800

    provide a example for testng

---
+++ src/test/java/test/NewTest.java
@@ -0,0 +1,39 @@
+package test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	WebDriver driver = null;
+	String url = "";
+
+	@BeforeTest
+	public void setupEnv() {
+		// check if firefox driver runs successfully on your computer
+		// System.setProperty("webdriver.gecko.driver", "/home/user/bin");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		this.url = "https://www.baidu.com/";
+	}
+
+	@Test(invocationCount=2,threadPoolSize=2)
+	public void checkEnv() {
+		// driver = new FirefoxDriver();
+		driver = new ChromeDriver();
+		driver.get(this.url);
+		WebElement we = driver.findElement(By.cssSelector("#kw"));
+		we.sendKeys("testNG");
+		we.click();
+		System.out.println(we.getAttribute("name")+"｜"+Thread.currentThread().getId());
+	}
+
+	@AfterTest
+	public void doClose() {
+		 driver.quit();
+		// driver.close();
+	}
+
+}
